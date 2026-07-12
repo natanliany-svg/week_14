0b653294be4adf221e96744bc5b56df9b88152b7
Natan Liany <natanliany@gmail.com>
2026-07-12 16:12:24 +0300

    -

---
+++ project_Restaurant_Order/data/manageJson.js
@@ -0,0 +1,35 @@
+
+import fs from "fs/promises"
+
+
+const JSON_PATH = process.env.JSON_PATH
+
+async function writeToJson(params) {
+    
+
+    try{
+        await fs.
+
+    }
+
+    catch{
+
+    }
+
+}
+
+
+
+
+
+
+async function getDataFromJson(params) {
+    
+    try{
+
+    }
+
+    catch{
+        
+    }
+}
